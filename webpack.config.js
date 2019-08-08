c3e0967a991ab9dd6d75ccf528520e3995bab67e
Fernando Tirelli <fernando.tirelli@gmail.com>
2019-08-08 11:34:24 -0300

    Version 1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[contentHash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
