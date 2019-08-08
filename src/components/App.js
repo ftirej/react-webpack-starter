6742674adf6b88aba0a51337209aac4987b57b28
Fernando Tirelli <fernando.tirelli@gmail.com>
2019-08-08 12:48:14 -0300

    add eslint, prettier

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    // Remove this line
+    console.log('Webpack Starter');
     return <h1>WebPack Starter</h1>;
   }
 }
